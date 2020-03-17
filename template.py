fe590db7d04c906463ca8253b559d5e882a52ecb
Cyrill Martin <cyrill.martin@unibas.ch>
2020-03-17 15:57:44 +0100

    added variable parts, named parameters, POST, GET

--- template.py
+++ template.py
@@ -3,11 +3,15 @@ import os
 import socket
 import requests, json
 from random import random
-from flask import jsonify
+from flask import jsonify, request, redirect, url_for
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+##############
+# Basic routes
+##############
 
 @app.route('/')
 def hello():
@@ -28,6 +32,59 @@ def randomNumber():
         email='test@test.com',
         id=random()
     )
+
+################
+# Variable parts
+################
+
+# String
+@app.route('/hello/<name>')
+def hello_name(name):
+    return 'Hello %s!' % name.capitalize()
+
+# Integer
+@app.route('/item/<int:itemID>')
+def show_item(itemID):
+   return 'Item number %d:' % itemID
+
+# Float
+@app.route('/version/<float:versionNo>')
+def show_version(versionNo):
+   return 'Version number %f:' % versionNo
+
+##################
+# Named parameters
+##################
+
+@app.route('/hero')
+def show_hero():
+    name = request.args.get('name', default = 'Nobody', type = str)
+    return '%s will save you!' % name.capitalize()
+
+    # /hero?name=batman
+    # --> Batman will save you!
+
+    # /hero
+    # --> Nobody will save you!
+
+##############
+# HTTP methods
+##############
+
+# POST & GET
+# ...also introducing redirect and url_for
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      name = request.form['name']
+      return redirect(url_for('hello_name', name = name))
+   else:
+      name = request.args.get('name')
+      return redirect(url_for('hello_name', name = name))
+
+      # POST: run template.py and open template.html in browser
+      # GET: /login?name=superman
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
